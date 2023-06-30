6f83866ca3652ef92da7c8290f2fb0951d745115
Mariselvamchellaiya <mariselvamchellaiya@gmail.com>
2023-06-30 08:16:13 +0530

    change2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Link, Route } from "react-router-dom";
+import Userlists from "./Components/Userlists";
+import Adduser from "./Components/Adduser";
+import { ToastContainer } from "react-toastify";
+import Home from "./Components/Home";
+import store from "./Redux/Store";
+import { Provider } from "react-redux";
+import Treeview2 from "./Components/Treeview2";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div>
+        <Router>
+          <div className="header">
+            <Link to={"/"}>Home</Link>
+            <Link to={"/user"}>User</Link>
+          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/user" element={<Userlists />} />
+            <Route path="/user/add" element={<Adduser />} />
+            <Route path="user/treeview/:id" element={<Treeview2 />} />
+          </Routes>
+        </Router>
+        <ToastContainer></ToastContainer>
+      </div>
+    </Provider>
   );
 }
 
